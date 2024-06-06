c671f8a2417ebc1e780166d6fccb9a92434b9121
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-06 21:30:56 +0300

    fix: cartPanel component becomes panel and is rewritten to work with zustand

---
+++ app/components/panel.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
+import Quantity from "./quantity";
+import { CartItem, Game } from "../lib/definitions";
+import { useStore } from "../store";
+
+export default function Panel({ game }: { game: Game }) {
+  const storedGame = useStore((store) => store.cart).find(
+    (cartItem: CartItem) => cartItem.id == game.id
+  );
+  const addCartItem = useStore((store) => store.addCartItem);
+  const [quantity, setQuantity] = useState<number>(2);
+  useEffect(() => {
+    if (storedGame) {
+      setQuantity(Number(storedGame.quantity));
+    } else {
+      setQuantity(1);
+    }
+  }, []);
+  return (
+    <>
+      <Quantity quantity={quantity} setQuantity={setQuantity} />
+      <div className="action-btns">
+        <button
+          className="btn"
+          onClick={() =>
+            addCartItem({
+              id: game.id,
+              title: game.title,
+              price: game.price,
+              imageurl: game.imageurl,
+              quantity,
+            })
+          }
+        >
+          Add to cart
+        </button>
+      </div>
+    </>
+  );
+}
