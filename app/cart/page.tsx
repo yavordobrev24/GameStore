e9ca8393b7c042cd0700ebcb54dba93b1c9cd3c5
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-07 23:10:11 +0300

    fix: Cart component now has the logic for the checkout if user is logged

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,10 +1,22 @@
 import CartList from "../components/cartList";
+import CheckoutButton from "../components/checkoutButton";
+import Newest from "../components/newest";
+import { getSession } from "../lib/session";
 
-export default function Cart() {
+export default async function Cart() {
+  const session = await getSession();
   return (
     <main className="flex-1 flex flex-col main-section">
-      <h2 className="text-3xl">Shopping Cart</h2>
-      <CartList />
+      <section>
+        <h2 className="text-3xl">Shopping Cart</h2>
+        <CartList />
+        {session ? (
+          <CheckoutButton />
+        ) : (
+          <p className="text-center text-lg">Login to checkout</p>
+        )}
+      </section>
+      <Newest />
     </main>
   );
 }
