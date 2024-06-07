78b2c6640476ebd7b4d42406a9d9ee91ae355f42
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-07 23:08:30 +0300

    fix: GamePage added Newest component and removed logic for not logged in users

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -1,9 +1,8 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import pool from "../../../postgres-db/db";
-import Quantity from "@/app/components/quantity";
 import { Game } from "@/app/lib/definitions";
 import Panel from "@/app/components/panel";
-import { getSession } from "@/app/lib/session";
+import Newest from "@/app/components/newest";
 
 async function getGame(gameId: number) {
   const res = await pool.query("SELECT * FROM games WHERE id = $1", [gameId]);
@@ -12,25 +11,22 @@ async function getGame(gameId: number) {
 
 export default async function GamePage({ params }: Params) {
   const game: Game = await getGame(params.id);
-  const session = await getSession();
 
   return (
-    <main className="flex-1 flex details">
-      <section className="details-img">
-        <img src={game.imageurl} alt={game.title} />
-      </section>
-      <section className="details-content">
-        <h1 className="name">{game.title}</h1>
-        <p className="desc">{game.description}</p>
-        <p className="price">${game.price}</p>
-        {session ? (
+    <>
+      <main className="flex-1 flex details">
+        <section className="details-img">
+          <img src={game.imageurl} alt={game.title} />
+        </section>
+        <section className="details-content">
+          <h1 className="name">{game.title}</h1>
+          <p className="desc">{game.description}</p>
+          <p className="price">${game.price}</p>
+
           <Panel game={game} />
-        ) : (
-          <p className="text-gray-500 mt-4 font-semibold">
-            Please log in to continue shopping!
-          </p>
-        )}
-      </section>
-    </main>
+        </section>
+      </main>
+      <Newest />
+    </>
   );
 }
