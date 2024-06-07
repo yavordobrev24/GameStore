03efc216cbfb10bc095620d3129ca5209f0b8e67
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-08 00:29:40 +0300

    fix: footer sitemap shortened

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,19 +2,14 @@ import Link from "next/link";
 
 export default async function Footer() {
   return (
-    <footer className=" bg-primary text-secondary px-[10rem] p-[3rem] w-full border-t-[1px] border-gray-300">
+    <footer className="flex justify-center bg-primary text-secondary px-[10rem] p-[3rem] w-full border-t-[1px] border-gray-300 content-center">
       <nav className="flex gap-[5rem]">
-        <div>
-          <h2 className="text-lg font-bold">Sitemap</h2>
-          <ul>
+        <div className="flex flex-col gap-3">
+          <h2 className="text-lg font-bold text-center">Sitemap</h2>
+          <ul className="flex gap-3">
             <li>
               <Link href="/games?category=All">Games</Link>
             </li>
-          </ul>
-        </div>
-        <div>
-          <h2 className="text-lg font-bold">Category</h2>
-          <ul>
             <li>
               <Link href="/games?category=Action">Action</Link>
             </li>
